344bf2d2b68ced1c1f8f031ba9a2e37f970d809a
Valentino Traverso <vt20032003@live.com.ar>
2023-03-22 12:57:05 +0100

    feat(styles desktop): add desktop styles)

--- src/UI/Styles/Pages/Users/SettingsStyle.jsx
+++ src/UI/Styles/Pages/Users/SettingsStyle.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { colors } from "../../config";
+import { colors, device } from "../../config";
 
 export const ContainerSettings = styled.div`
 margin: auto;
+@media ${device.desktop}{
+    width: 50%;
+}
 `
 export const ContainerTitleSettings = styled.div`
 display: flex;
@@ -13,11 +16,20 @@ margin: 10px 0;
 export const SubClassSettingsTitle = styled.h3`
     font-size:1.3rem;
     font-weigth: 800;
+    @media ${device.desktop}{
+        font-size:1.6rem;
+    }
 `
 export const TitleSetting = styled.span`
+@media ${device.desktop}{
+    font-size:1.2rem;
+}
 `
 export const LogOutButton = styled.span`
 position: absolute;
     bottom: 20px;
     color:${colors.grey}
+    @media ${device.desktop}{
+        font-size:1.2rem;
+    }
     `
