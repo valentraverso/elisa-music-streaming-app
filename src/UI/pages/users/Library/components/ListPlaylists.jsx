b401041245cdee7761004d10f2be900b79941166
Albert Gomis <albert.gomis.20@gmail.com>
2023-03-22 11:36:22 +0100

    feat: link from Library to playlist

--- src/UI/pages/users/Library/components/ListPlaylists.jsx
+++ src/UI/pages/users/Library/components/ListPlaylists.jsx
@@ -1,17 +1,22 @@
 import { GeneralDiv, PageTitle, DivTypePlaylist,Subtitle, ImgPlaylist, DivInfoPlaylist, TitlePlaylist, DivAllPlaylist} from "../../../../Styles/Pages/Users/MenuPlaylistsStyle";
 import { PlaylistDisplay } from "./PlaylistDisplay";
-import { useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 export const ListPlaylists = ({name, data}) => {
-  const navigate = useNavigate();
   return (
     <GeneralDiv>
         <DivTypePlaylist>
             <Subtitle>{name}</Subtitle>
             <DivAllPlaylist>
-              <PlaylistDisplay name = "Sad Playlist" onClick ={navigate("/playlist")}/>
-              <PlaylistDisplay name ="Chill Playlist" />
-              <PlaylistDisplay name ="Rock Playlist"/>
+              <Link to = "/playlist">
+                <PlaylistDisplay name = "Sad Playlist"/>
+              </Link>
+              <Link to = "/playlist">
+                <PlaylistDisplay name ="Chill Playlist" />
+              </Link>
+              <Link to = "/playlist">
+              < PlaylistDisplay name ="Rock Playlist"/>
+              </Link>
             </DivAllPlaylist>
         </DivTypePlaylist>
     </GeneralDiv>
