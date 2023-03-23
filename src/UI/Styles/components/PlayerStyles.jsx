d8b3d7a251312423b4bff76c9e37631cd01ffe49
Albert Gomis <albert.gomis.20@gmail.com>
2023-03-23 17:22:29 +0100

    function: player in page

---
+++ src/UI/Styles/components/PlayerStyles.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const ContainerPlayer = styled.div`
+    display: grid;
+    grid-template-columns: 60% 40%;
+    border-radius: 10px;
+    position: fixed;
+    bottom: 60px;
+    left: 0px;
+    right: 0px;
+`
+
+export const ContainerInfoSong = styled.div`
+    width: auto;
+    display: flex;
+    flex-direction: row;
+`
+
+export const ImgInfoSong = styled.img`
+    width: 53px;
+    height: 53px;
+    border-radius: 5px;
+`
+
--- src/UI/components/Player/Player.jsx
+++ src/UI/components/Player/Player.jsx
@@ -1,24 +1,43 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import "./player.css"
-import { DivPlaylistsDisplay, ImgPlaylist, DivInfoPlaylist, TitlePlaylist, InfoPlaylist, DivAllPlaylist} from "../../Styles/Pages/Users/MenuPlaylistsStyle"
-
+// import { DivPlaylistsDisplay, ImgPlaylist, DivInfoPlaylist, TitlePlaylist, InfoPlaylist, DivAllPlaylist} from "../../Styles/Pages/Users/MenuPlaylistsStyle"
+import { ContainerPlayer, ContainerInfoSong, ImgInfoSong } from '../../Styles/components/PlayerStyles';
+import UseWidth from '../../../helpers/hooks/useWidth';
 
 export const Player = () => {
+
+  const width = UseWidth();
+
   return (
-    <div className='generalDivPlayer'>
-      <div className='divImgName'>
-        <img src="https://i.scdn.co/image/ab67616d0000b27396384c98ac4f3e7c2440f5b5" className='imgPlayer'/>
+    <ContainerPlayer>
+      <ContainerInfoSong>
+        <ImgInfoSong src="https://i.scdn.co/image/ab67616d0000b27396384c98ac4f3e7c2440f5b5" />
         <div className='divNameArtist'>
             <p className='pTitle'>Temazo</p>
             <p className='pArtist'>La Rosi</p>
         </div>
-      </div>
+      </ContainerInfoSong>
 
     <AudioPlayer
       src="/assets/songs/ZOO-INTRO.mp3"
+      showDownloadProgress={
+        width !== "desktop" && false
+      }
+      showFilledProgress={
+        width !== "desktop" && false
+      }
+      showJumpControls={
+        width !== "desktop" && false
+      }
+      layout="horizontal"
+      customAdditionalControls={[]}
+      showSkipControls= {true}
+      customVolumeControls = {[]}
+      customProgressBarSection={[]}
+      style={{padding: 0, backgroundColor: "none"}}
     />
 
-    </div>
+    </ContainerPlayer>
   )
 }
