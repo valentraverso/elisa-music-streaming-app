f25611da3f53fecfd6cb9bcb628e088755a44ed5
Albert Gomis <albert.gomis.20@gmail.com>
2023-03-23 16:10:12 +0100

    feat: player in page component 1.0

--- src/UI/components/Player/Player.jsx
+++ src/UI/components/Player/Player.jsx
@@ -1,12 +1,24 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import "./player.css"
+import { DivPlaylistsDisplay, ImgPlaylist, DivInfoPlaylist, TitlePlaylist, InfoPlaylist, DivAllPlaylist} from "../../Styles/Pages/Users/MenuPlaylistsStyle"
+
 
 export const Player = () => {
   return (
+    <div className='generalDivPlayer'>
+      <div className='divImgName'>
+        <img src="https://i.scdn.co/image/ab67616d0000b27396384c98ac4f3e7c2440f5b5" className='imgPlayer'/>
+        <div className='divNameArtist'>
+            <p className='pTitle'>Temazo</p>
+            <p className='pArtist'>La Rosi</p>
+        </div>
+      </div>
+
     <AudioPlayer
-    autoPlay
-    src="http://example.com/audio.mp3"
-    onPlay={e => console.log("onPlay")}
-  />
+      src="/assets/songs/ZOO-INTRO.mp3"
+    />
+
+    </div>
   )
 }
