2684960dc3a6e850d82487205ac70738292ed0cd
Valentino Traverso <vt20032003@live.com.ar>
2023-03-24 14:25:52 +0100

    fix layout main

--- src/UI/layouts/Users/LayoutMain/LayoutMain.jsx
+++ src/UI/layouts/Users/LayoutMain/LayoutMain.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router";
 import { Main } from "../../../Styles/LayoutsStyles/GeneralLayoutStyles";
 import { NavbarMain } from "../Components/NavbarMain";
-import { NavbarDesktop } from "../Components/NavbarDesktop";
 import { SideBar } from "../Components/SideBar";
-import { SideBarDesktop } from "../Components/SideBarDesktop";
 import UseWidth from "../../../../helpers/hooks/useWidth";
 import { CategoryeButtons } from "../Components/HomeComponents/CategoryButtons";
-import { BarLibraryDesktop } from "../Components/BarLibraryDesktop";
 import MenuDesktop from "../Components/MenuDesktop";
 
 
@@ -18,6 +15,7 @@ function LayoutMain() {
             <>
                 <MenuDesktop />
                 <Main>
+                    <CategoryeButtons />
                     <Outlet />
                 </Main>
             </>
