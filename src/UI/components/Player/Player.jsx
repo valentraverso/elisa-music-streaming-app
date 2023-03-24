21cd8aa5b67376f2a588a86d0ef8fac3e33ca7dd
Valentino Traverso <vt20032003@live.com.ar>
2023-03-24 11:55:02 +0100

    feat(player desktop)

--- src/UI/components/Player/Player.jsx
+++ src/UI/components/Player/Player.jsx
@@ -2,7 +2,7 @@ import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import "./player.css"
 // import { DivPlaylistsDisplay, ImgPlaylist, DivInfoPlaylist, TitlePlaylist, InfoPlaylist, DivAllPlaylist} from "../../Styles/Pages/Users/MenuPlaylistsStyle"
-import { ContainerPlayer, ContainerInfoSong, ImgInfoSong, ContainerDataSong, SpanSongTitle, SpanSongArtist} from '../../Styles/components/PlayerStyles';
+import { ContainerPlayer, ContainerInfoSong, ImgInfoSong, ContainerDataSong, SpanSongTitle, SpanSongArtist } from '../../Styles/components/PlayerStyles';
 import UseWidth from '../../../helpers/hooks/useWidth';
 import { colors } from '../../Styles/config';
 
@@ -15,30 +15,43 @@ export const Player = () => {
       <ContainerInfoSong>
         <ImgInfoSong src="https://i.scdn.co/image/ab67616d0000b27396384c98ac4f3e7c2440f5b5" />
         <ContainerDataSong>
-            <SpanSongTitle>Temazo</SpanSongTitle>
-            <br/>
-            <SpanSongArtist>La Rosi</SpanSongArtist>
+          <SpanSongTitle>Temazo</SpanSongTitle>
+          <br />
+          <SpanSongArtist>La Rosi</SpanSongArtist>
         </ContainerDataSong>
       </ContainerInfoSong>
 
-    <AudioPlayer
-      src="/assets/songs/ZOO-INTRO.mp3"
-      showDownloadProgress={
-        width !== "desktop" && false
-      }
-      showFilledProgress={
-        width !== "desktop" && false
-      }
-      showJumpControls={
-        width !== "desktop" && false
-      }
-      layout="horizontal"
-      customAdditionalControls={[]}
-      showSkipControls= {true}
-      customVolumeControls = {[]}
-      customProgressBarSection={[]}
-      style={{padding: 0, backgroundColor: colors.grey, boxShadow: "none", width: "98%"}}
-    />
+      {
+        width !== 'desktop' ?
+          <AudioPlayer
+            src="/assets/songs/ZOO-INTRO.mp3"
+            showDownloadProgress={
+              width !== "desktop" && false
+            }
+            showFilledProgress={
+              width !== "desktop" && false
+            }
+            showJumpControls={
+              width !== "desktop" && false
+            }
+            layout="horizontal"
+            customAdditionalControls={[]}
+            showSkipControls={true}
+            customVolumeControls={[]}
+            customProgressBarSection={[]}
+            style={{ padding: 0, backgroundColor: colors.grey, boxShadow: "none", width: "98%" }}
+          />
+          :
+          <AudioPlayer 
+          src="https://res.cloudinary.com/dppekhvoo/video/upload/v1679583767/albums/1/Hey_Mom_wv8nju.mp4"
+          layout="horizontal"
+          showSkipControls
+          showJumpControls={false}
+          showDownloadProgress={false}
+          customAdditionalControls={[]}
+          style={{ padding: 0, backgroundColor: colors.grey, boxShadow: "none", width: "98%" }}/>
+  }
+
 
     </ContainerPlayer>
   )
