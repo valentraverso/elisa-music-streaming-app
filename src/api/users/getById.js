a7fd56b107316c878a1a876817198e13026ddece
Valentino Traverso <vt20032003@live.com.ar>
2023-05-04 19:04:43 +0200

    add: fetch user by id

---
+++ src/api/users/getById.js
@@ -0,0 +1,18 @@
+const fetchSongByTitle = async (id, token) =>{
+
+    try {
+        const request = await fetch(`${process.env.REACT_APP_API_URL}/users/title/${id}`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const response =await request.json()
+        return response
+    } catch (error) {
+        console.error(error)
+        return {msg: error.message, token}
+    }
+}
+
+export default fetchSongByTitle
