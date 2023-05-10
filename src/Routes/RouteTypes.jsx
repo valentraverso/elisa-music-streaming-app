a15c9bd06e74716817e80bc1a8ba812a80f515b7
Valentino Traverso <vt20032003@live.com.ar>
2023-05-10 16:59:21 +0200

    feat: optimize private routes

--- src/Routes/RouteTypes.jsx
+++ src/Routes/RouteTypes.jsx
@@ -1,18 +1,51 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import ProviderVerifyUser from "../context/ProviderVerifyUser";
+import { useQuery } from "react-query";
+import { store } from "../utils/redux/store";
+import getUserBySub from "../api/users/getBySub";
+import { ADD_DATA_USER } from "../utils/redux/reducers/user";
 
 const PrivateRoutes = ({ children }) => {
-    const { isAuthenticated } = useAuth0();
+    const { isLoading: isLoadingAuth, isAuthenticated, getAccessTokenSilently } = useAuth0();
     const navigate = useNavigate();
 
+    const reduxUser = store.getState().user;
+
+    const userAction = async (user) => {
+        switch (user?.status) {
+            case true:
+                store.dispatch(ADD_DATA_USER(user));
+                navigate('/');
+                break;
+            default:
+                navigate('/register');
+                break;
+        }
+    }
+
+    const { isLoading } = useQuery(['user'], async () => {
+        const token = await getAccessTokenSilently();
+
+        const data = await getUserBySub(token);
+
+        await userAction(data);
+
+        return data;
+    })
+
+    console.log(reduxUser)
+
     return (
-        isAuthenticated ?
-            <ProviderVerifyUser>
-                {children}
-            </ProviderVerifyUser>
+        isLoadingAuth || isLoading ?
+            <p>Loading...</p>
             :
-            navigate('/login')
+            (
+                isAuthenticated ?
+                    children
+
+                    :
+                    navigate('/login')
+            )
     )
 }
 
