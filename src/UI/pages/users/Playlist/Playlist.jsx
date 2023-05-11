3b02ccd364a8b1bd35ec801ee1500467a89c96aa
Valentino Traverso <vt20032003@live.com.ar>
2023-05-11 13:23:36 +0200

    feat: albums

--- src/UI/pages/users/Playlist/Playlist.jsx
+++ src/UI/pages/users/Playlist/Playlist.jsx
@@ -4,13 +4,13 @@ import { PlaylistTitle } from "./components/PlaylistTitle/PlaylistTitle";
 import { SongsList } from "./components/SongsList/SongsList";
 
 export const Playlist = ({ data }) => {
-  const { img, title } = data;
+  const { img: {secure_url: imgAlbum}, title } = data;
 
   return (
     <ContainerPagePlaylist>
       <ContainerPlaylist >
-        <PlaylistTitle img={img.secure_url} playlistName={title} info={""}  likes={""} btnLike={true} />
-        <SongsList data={data.songs} />
+        <PlaylistTitle img={imgAlbum} playlistName={title} info={""}  likes={""} btnLike={true} />
+        <SongsList data={data.songs} imgAlbum={imgAlbum} />
       </ContainerPlaylist>
       <ButtonAddSong> + Add new Song</ButtonAddSong>
     </ ContainerPagePlaylist>
