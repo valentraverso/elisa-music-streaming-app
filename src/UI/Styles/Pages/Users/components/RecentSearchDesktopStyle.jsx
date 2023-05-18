7e286b0e01fa3a4dccb9ecc52e34d090862ed23f
Ynohe Sandoval <ynotimelesss@gmail.com>
2023-05-18 10:41:04 +0100

    feat: add recent search

--- src/UI/Styles/Pages/Users/components/RecentSearchDesktopStyle.jsx
+++ src/UI/Styles/Pages/Users/components/RecentSearchDesktopStyle.jsx
@@ -1,49 +1,42 @@
 import styled from "styled-components";
 
-
 export const ContainerRecentSearchDesktop = styled.div`
-
-`
+  /* Styles for the recent search container */
+`;
 
 export const H2RecentTitleDesktop = styled.span`
     font-weight: bold;
     font-size: 1.5rem;
     margin-top: 0;
-`
+`;
 
 export const ContainerRecentDesktop = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-wrap: wrap; /* Allow albums to wrap to the next row */
     gap: 4%;
     margin-top: 2%;
     margin-bottom: 10%;
-`
-
+`;
 
 export const RecentImgDesktop = styled.img`
-    width: 120px;;
+    width: 120px;
     height: 120px;
     display: block;
     border-radius: 5px;
-`
-
-export const ContainerRecentImgDesktop = styled.div`
-    
-`
+`;
 
 export const RecentDescriptionDesktop = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-`
-
+    align-items: center;
+    text-align: center;
+    width: 120px; /* Set a fixed width for each album */
+`;
 
 export const Title = styled.p`
     font-weight: bold;
-`
+`;
 
 export const Artist = styled.p`
     font-size: 14px;
-`
-
-
+`;
--- src/UI/pages/users/Search/components/RecentSearchDesktop.jsx
+++ src/UI/pages/users/Search/components/RecentSearchDesktop.jsx
@@ -1,25 +1,79 @@
 import { Artist, ContainerRecentDesktop, ContainerRecentSearchDesktop, H2RecentTitleDesktop, RecentDescriptionDesktop, RecentImgDesktop, Title, } from "../../../../Styles/Pages/Users/components/RecentSearchDesktopStyle"
-import { recentSearch } from "../recentSearch"
+// import { recentSearch } from "../recentSearch"
 import Category from "./Category";
 
-export default function RecentSearchDesktop() {
+// export default function RecentSearchDesktop() {
+//     return (
+//         <>
+//             <ContainerRecentSearchDesktop>
+//                 <H2RecentTitleDesktop>Recents</H2RecentTitleDesktop>
+//                 <ContainerRecentDesktop>
+//                     {recentSearch.map((recentSearch) => {
+//                         return (
+//                             <RecentDescriptionDesktop>
+//                                 <RecentImgDesktop src={recentSearch.img} />
+//                                 <Title>{recentSearch.title}</Title>
+//                                 <Artist>{recentSearch.artist}</Artist>
+//                             </RecentDescriptionDesktop>
+//                         )
+//                     })}
+//                 </ContainerRecentDesktop>
+//             </ContainerRecentSearchDesktop>
+//             <Category />
+//         </>
+//     )
+// }
+import React from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import fetchAllAlbums from "../../../../../api/albums/fetchAllAlbums";
+import { useAuth0 } from "@auth0/auth0-react";
+import { ContainerAlbum, AlbumBackground, Album, AlbumTitle, AlbumImage } from "../../../../Styles/Pages/Users/HomePageStyles/AlbumsStyle";
+import { Skeleton } from "antd";
+
+const RecentSearchDesktop = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const { data: albums, isLoading } = useQuery(["allAlbums"], async () => {
+        const token = await getAccessTokenSilently();
+        const data = await fetchAllAlbums(token);
+        return data;
+    });
+
+    const limitedAlbums = albums?.slice(0, 6);
+
     return (
-        <>
+        <div>
+            <h2>Recent Search</h2>
             <ContainerRecentSearchDesktop>
-                <H2RecentTitleDesktop>Recents</H2RecentTitleDesktop>
-                <ContainerRecentDesktop>
-                    {recentSearch.map((recentSearch) => {
-                        return (
+                {isLoading ? (
+                    <Skeleton>
+                        <ContainerRecentDesktop>
                             <RecentDescriptionDesktop>
-                                <RecentImgDesktop src={recentSearch.img} />
-                                <Title>{recentSearch.title}</Title>
-                                <Artist>{recentSearch.artist}</Artist>
+                                <RecentImgDesktop />
+                                <Title />
                             </RecentDescriptionDesktop>
-                        )
-                    })}
-                </ContainerRecentDesktop>
+                        </ContainerRecentDesktop>
+                    </Skeleton>
+                ) : (
+                    limitedAlbums &&
+                    limitedAlbums.map((album) => (
+                        <ContainerRecentDesktop key={album._id}>
+                            <Link to={`/album/${album._id}`}>
+                                <RecentDescriptionDesktop>
+                                    <RecentImgDesktop
+                                        src={album.img && album.img.secure_url}
+                                        alt={album.title}
+                                    />
+                                    <Title>{album.title}</Title>
+                                </RecentDescriptionDesktop>
+                            </Link>
+                        </ContainerRecentDesktop>
+                    ))
+                )}
             </ContainerRecentSearchDesktop>
-            <Category />
-        </>
-    )
-}
+        </div>
+    );
+};
+
+export default RecentSearchDesktop;
