67d5cbc8ce3bab9927327d353c08689f011520b7
Ynohe Sandoval <ynotimelesss@gmail.com>
2023-05-18 14:37:57 +0100

    feat: add recent search for mobile version

--- src/UI/pages/users/Search/components/RecentSearch.jsx
+++ src/UI/pages/users/Search/components/RecentSearch.jsx
@@ -1,38 +1,70 @@
-import { recentSearch } from "../recentSearch"
-import { ContainerRecentSearch, ContainerRecent, RecentImg, RecentDescription, Title, Artist, DeleteRecentSearch, DeleteHistory, H1RecentTitle, ContainerRecentImg, ContainerDeleteHistory, SpanDeleteRecentSearch , ContainerRecentSearchAll} from "../../../../Styles/Pages/Users/components/RecentBarStyle"
+import React from "react";
+import { ContainerRecentSearch, ContainerRecent, RecentImg, RecentDescription, Title, Artist, DeleteRecentSearch, DeleteHistory, H1RecentTitle, ContainerRecentImg, ContainerDeleteHistory, SpanDeleteRecentSearch, ContainerRecentSearchAll } from "../../../../Styles/Pages/Users/components/RecentBarStyle";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import fetchAllAlbums from "../../../../../api/albums/fetchAllAlbums";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Skeleton } from "antd";
 
+const RecentSearch = () => {
+    const { getAccessTokenSilently } = useAuth0();
 
+    const { data: albums, isLoading } = useQuery(["allAlbums"], async () => {
+        const token = await getAccessTokenSilently();
+        const data = await fetchAllAlbums(token);
+        return data;
+    });
 
+    const limitedAlbums = albums?.slice(0, 6);
 
-export const RecentSearch = () => {
     return (
         <ContainerRecentSearch>
             <H1RecentTitle>Recents</H1RecentTitle>
-                    <ContainerRecent>
-                    {recentSearch.map((recentSearch) => {
-                return (
+            <ContainerRecent>
+                {isLoading ? (
+                    <Skeleton>
                         <ContainerRecentSearchAll>
-                        <ContainerRecentImg>
-                            <RecentImg src={recentSearch.img} />
-                        </ContainerRecentImg>
-                        <RecentDescription>
-                            <Title>{recentSearch.title}</Title>
-                            <Artist>{recentSearch.artist}</Artist>
-                        </RecentDescription>
-                        <DeleteRecentSearch>
-                            <SpanDeleteRecentSearch>
-                                X
-                            </SpanDeleteRecentSearch>
-                        </DeleteRecentSearch>
+                            <ContainerRecentImg>
+                                <RecentImg />
+                            </ContainerRecentImg>
+                            <RecentDescription>
+                                <Title />
+                                <Artist />
+                            </RecentDescription>
+                            <DeleteRecentSearch>
+                                <SpanDeleteRecentSearch>X</SpanDeleteRecentSearch>
+                            </DeleteRecentSearch>
                         </ContainerRecentSearchAll>
-                )
-            })}
+                    </Skeleton>
+                ) : (
+                    limitedAlbums &&
+                    limitedAlbums.map((album) => (
+                        <ContainerRecentSearchAll key={album._id}>
+                            <ContainerRecentImg>
+                                <RecentImg
+                                    src={album.img && album.img.secure_url}
+                                    alt={album.title}
+                                />
+                            </ContainerRecentImg>
+                            <RecentDescription>
+                                <Link to={`/album/${album._id}`}>
+                                    <Title>{album.title}</Title>
+                                    <Artist>{album.artist}</Artist>
+                                </Link>
+                            </RecentDescription>
+                            <DeleteRecentSearch>
+                                <SpanDeleteRecentSearch>X</SpanDeleteRecentSearch>
+                            </DeleteRecentSearch>
+                        </ContainerRecentSearchAll>
+                    ))
+                )}
             </ContainerRecent>
             <ContainerDeleteHistory>
-                <DeleteHistory>
-                    Delete History
-                </DeleteHistory>
+                <DeleteHistory>Delete History</DeleteHistory>
             </ContainerDeleteHistory>
         </ContainerRecentSearch>
-    )
-}
+    );
+};
+
+export default RecentSearch;
+
